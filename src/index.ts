2f8c4d31037870239fec17019d166acc92f36be3
Rinto Jose <rintoj@gmail.com>
2025-08-08 03:25:32 +0530

    refactor: reorganize imports and exports in index.ts for clarity

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export * from './agent/index.js'
 export * from './agent-message/index.js'
-export * from './model/index.js'
-export * from './tool/index.js'
+
+import * as tools from './tool/index.js'
+import * as models from './model/index.js'
+
+export { tools, models }
