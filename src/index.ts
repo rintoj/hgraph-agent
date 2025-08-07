26e797ed5ca13c9b4b602cf0e72bfde69277c512
Rinto Jose <rintoj@gmail.com>
2025-08-08 03:29:10 +0530

    chore: Rearrange import statement for tools

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from './agent/index'
 export * from './agent-message/index'
 
-import * as tools from './tool/index'
 import * as models from './model/index'
+import * as tools from './tool/index'
 
 export { tools, models }
